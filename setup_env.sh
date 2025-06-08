d3753e3c5bd46cf45dc6c4eef2552e564004e5b0
VarshithMittapalli <varshith222@gmail.com>
2025-06-08 18:04:54 +0000

    updated sh file

--- setup_env.sh
+++ setup_env.sh
@@ -2,11 +2,16 @@
 
 set -e
 
-apt-get update && apt-get install -y python3-pip
+echo "🔧 Updating package list and installing Python3 and pip..."
+sudo apt update
+sudo apt install -y python3 python3-pip python3-venv
+
 echo "Creating Virtual environment"
 python3 -m venv .venv
+source .venv/bin/activate
 
 echo "Downloading dependencies"
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Running Flake8 check"
